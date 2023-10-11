a4aded5c1b89c68882287870c485bc1727c42e37
Jey <126204426+Jeyrama@users.noreply.github.com>
2023-10-11 13:09:51 -0700

    Update Is-a-number-prime.js

--- Is-a-number-prime.js
+++ Is-a-number-prime.js
@@ -19,3 +19,14 @@ Examples:
 
 
 // Solution
+
+function isPrime(num) {
+  if (num < 2) return false;
+  const limit = Math.sqrt(num);
+  for (let i = 2; i <= limit; ++i) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+  return true;
+}
